83a113f5bcd2e6c26d2e1e5d3d5ca06e21b95587
ejerhamre <ejerhamre@gmail.com>
2019-01-06 22:03:05 +0100

    problem med karta

--- public/js/vue_script.js
+++ public/js/vue_script.js
@@ -6,13 +6,26 @@ var vm = new Vue({
   data: {
     orders: [],
     burgerOrders: [],
+    customerInfo: [],
     message: "hello",
     name: "",
     email: "",
     payment: "",
     gender: "",
+    details: {},
+    location: {x: "", y:""},
+    show: true,
     arbitraryVariableName: 'Välj din burgare',
     burgers: food,
+  },
+  created: function () {
+    socket.on('initialize', function (data) {
+      this.orders = data.orders;
+    }.bind(this));
+
+    socket.on('currentQueue', function (data) {
+      this.orders = data.orders;
+    }.bind(this));
   },
     methods: {
       markDone: function() {
@@ -23,25 +36,27 @@ var vm = new Vue({
           var lastOrder = Object.keys(this.orders).reduce( function (last, next) {
             return Math.max(last, next);
           }, 0);
-          return lastOrder + 1;
+          return "T";
+      },
+      addOrder: function (event) {
+        socket.emit("addOrder", { orderId: this.getNext(),
+                                  details: { x: event.clientX-10 - event.currentTarget.getBoundingClientRect().left,
+                                             y: event.clientY-10 - event.currentTarget.getBoundingClientRect().top},
+                                  orderItems: [this.burgerOrders],
+                                  customerInfo: [this.name, this.email, this.payment, this.gender]
+                                });
+      },
+      displayOrder: function (event) {
+        var offset = {x: event.currentTarget.getBoundingClientRect().left,
+                    y: event.currentTarget.getBoundingClientRect().top};
+                    this.details.x = event.clientX;
+                    this.details.y = event.clientY;
+                    /*this.location.x = {event.clientX - 10 - offset.x};
+                    this.location.y = {event.clientY - 10 - offset.y};*/
+                    this.location = { x: event.clientX - 10 - offset.x,
+                    y: event.clientY - 10 - offset.y };
       }
-    },
-   created: function () {
-     socket.on('initialize', function (data) {
-       this.orders = data.orders;
-     }.bind(this));
+   
 
-     socket.on('currentQueue', function (data) {
-       this.orders = data.orders;
-     }.bind(this));
-   },
-     addOrder: function (event) {
-       socket.emit("addOrder", { orderId: this.getNext(),
-                                 details: { x: event.clientX-10 - event.currentTarget.getBoundingClientRect().left,
-                                            y: event.clientY-10 - event.currentTarget.getBoundingClientRect().top},
-                                 orderItems: ["Beans", "Curry"]
-                               });
-     }
-
-
+}
 })
